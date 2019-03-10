da8d40e69b6dd3147a94b5fcb951216ffaf1bf03
marco7247 <43509851+marco7247@users.noreply.github.com>
2019-03-10 14:36:41 -0500

    feat: 'correction of routes'

--- TeacherAdminApp/src/app/app.routes.ts
+++ TeacherAdminApp/src/app/app.routes.ts
@@ -2,12 +2,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModulesComponent } from './components/core/containers/modules/modules.component';
 import { AsignaturesComponent } from './components/core/containers/asignatures/asignatures.component';
 import { ActivitiesComponent } from './components/core/containers/activities/activities.component';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './components/home/containers/login/login.component';
 
 export const ROUTES: Routes = [
+	{ path: 'home', component: LoginComponent },
 	{ path: 'modules', component: ModulesComponent },
 	{ path: 'asignatures', component: AsignaturesComponent },
-    { path: 'activities', component: ActivitiesComponent },
+	{ path: 'activities', component: ActivitiesComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 
+@NgModule({
+	imports: [
+		RouterModule.forRoot(
+			ROUTES,
+			
+		)
+	],
+	exports: [
+		RouterModule
+	]
+})
 export class AppRoutesModule { }
